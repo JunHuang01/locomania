5b7846c7997bd030142b61175ba4674838d6a98a
trevorprater <trevor.prater@gmail.com>
2015-04-01 04:35:06 -0400

    Added server.py logging.x

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from tornado.httpserver import HTTPServer
 import tornado.options
 import os
 
+LOGFILE = 'server.log'
+
 
 class VersionHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -20,11 +22,17 @@ class LatLongRecieverHandler(tornado.web.RequestHandler):
 		response = {'version':'0.0.2',
 					'last_build': date.today().isoformat()}
 		self.write(response)
+		f = open(LOGFILE, 'w+')
+		f.write(response)
+		f.close()
 	def post(self,pushCount,lat,long):
 		print "This is the {0}th push from device, and the data is Latitude: {1}, Longtitude: {2} ".format(pushCount,lat,long)
 		response = {'version':'0.0.3',
 					'last_build': date.today().isoformat()}
 		self.write(response)
+		f = open(LOGFILE, 'w+')
+		f.write(response)
+		f.close()
 
 Application = tornado.web.Application([
 		(r"/version",VersionHandler),
