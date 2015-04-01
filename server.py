df2f81316f187fb4234bd5524df7fb7b3de7b6fd
trevorprater <trevor.prater@gmail.com>
2015-04-01 04:57:07 -0400

    small fixes to server.py

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from tornado.httpserver import HTTPServer
 import tornado.options
 import os
 
+
+
 LOGFILE = 'server.log'
 
 
@@ -15,7 +17,6 @@ class VersionHandler(tornado.web.RequestHandler):
 					'last_build': date.today().isoformat()}
 		self.write(response)
 
-
 class LatLongRecieverHandler(tornado.web.RequestHandler):
 	
 	def get(self,pushCount,lat,long):
@@ -23,25 +24,14 @@ class LatLongRecieverHandler(tornado.web.RequestHandler):
 		response = {'version':'0.0.2',
 					'last_build': date.today().isoformat()}
 		self.write(response)
-		f = open(LOGFILE, 'w+')
-		time = str(datetime.now())
+		f = open(LOGFILE, 'a')
+		time = str(datetime.now())+ ': '
 		f.write(time + str(response))
-		f.close()
+		f.write(str(lat)+','+str(long))
 	
-	def post(self,pushCount,lat,long):
-		print "This is the {0}th push from device, and the data is Latitude: {1}, Longtitude: {2} ".format(pushCount,lat,long)
-		response = {'version':'0.0.3',
-					'last_build': date.today().isoformat()}
-		self.write(response)
-		f = open(LOGFILE, 'w+')
-		time = str(datetime.now())
-		f.write(time + str(response))
-		f.close()
-
 Application = tornado.web.Application([
 		(r"/version",VersionHandler),
 		(r"/pushLatLong/(.*)/(.*)/(.*)",LatLongRecieverHandler)])
-
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
