e0bbce5c9c58f90fb625697ee7efa0e27e26704d
Jun Huang <mightybeesun@gmail.com>
2015-04-01 02:23:40 -0400

    post to restAPI done

--- locomania/ViewController.swift
+++ locomania/ViewController.swift
@@ -23,12 +23,15 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
     
     let PUSH_INTERVAL = 15;
     
+    var pushCount: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
                 // Do any additional setup after loading the view, typically from a nib.
         self.setUpLocationManager();
         
         currTimeSinceLastPush = 0;
+        pushCount = 0;
         
         self.timerUpdate();
     }
@@ -67,6 +70,7 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
             self.currTimeSinceLastPush = self.currTimeSinceLastPush + 1;
             if (self.currTimeSinceLastPush > self.PUSH_INTERVAL ){
                 self.currTimeSinceLastPush = 0;
+                self.pushLocationToServer(self.currUserLocation);
             }
             self.Timer.text = String(self.currTimeSinceLastPush);
             self.timerUpdate();
@@ -74,5 +78,83 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
         
         
     }
+    
+    func pushLocationToServer(location: CLLocation){
+        self.pushCount = self.pushCount + 1;
+        
+        //TO-DO: Change the url here for your own API call
+        self.post(["":""], url: "http://192.168.1.110:8888/pushLatLong/\(self.pushCount)/\(location.coordinate.latitude)/\(location.coordinate.longitude)") { (succeeded: Bool, msg: String) -> () in
+            var alert = UIAlertView(title: "Success!", message: msg, delegate: nil, cancelButtonTitle: "Okay.")
+            if(succeeded) {
+                alert.title = "Success!"
+                alert.message = msg
+            }
+            else {
+                alert.title = "Failed :("
+                alert.message = msg
+            }
+            
+            // Move to the UI thread
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                // Show the alert
+                alert.show()
+            })
+        }
+    
+    }
+    
+    
+    
+    
+    /* https://github.com/jquave/SwiftPOSTTutorial */
+    func post(params : Dictionary<String, String>, url : String, postCompleted : (succeeded: Bool, msg: String) -> ()) {
+        var request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var err: NSError?
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+            
+            var msg = "No message"
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Error could not parse JSON: '\(jsonStr)'")
+                postCompleted(succeeded: false, msg: "Error")
+            }
+            else {
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    if let success = parseJSON["success"] as? Bool {
+                        println("Succes: \(success)")
+                        postCompleted(succeeded: success, msg: "Logged in.")
+                    }
+                    return
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("Error could not parse JSON: \(jsonStr)")
+                    postCompleted(succeeded: false, msg: "Error")
+                }
+            }
+        })
+        
+        task.resume()
+    }
+    
 }
 
