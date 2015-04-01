912f05e982463a793cf4074226493d6b6c1eae77
Jun Huang <mightybeesun@gmail.com>
2015-04-01 02:36:31 -0400

    remove alert and comments

--- locomania/ViewController.swift
+++ locomania/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
     
     var currTimeSinceLastPush: Int!
     
-    let PUSH_INTERVAL = 15;
+    let PUSH_INTERVAL = 15;  //Time in seconds for how long between push
     
     var pushCount: Int!
     
@@ -63,6 +63,8 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
         currUserLocation = location;
     }
     
+    
+    //recursive function that starts background thread every other 1 seconds to serve the time.
     func timerUpdate(){
         let delay = 1 * Double(NSEC_PER_SEC);
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay));
@@ -86,19 +88,21 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
         self.post(["":""], url: "http://192.168.1.110:8888/pushLatLong/\(self.pushCount)/\(location.coordinate.latitude)/\(location.coordinate.longitude)") { (succeeded: Bool, msg: String) -> () in
             var alert = UIAlertView(title: "Success!", message: msg, delegate: nil, cancelButtonTitle: "Okay.")
             if(succeeded) {
-                alert.title = "Success!"
-                alert.message = msg
+                println("Success push!")
+                //alert.title = "Success!"
+                //alert.message = msg
             }
             else {
-                alert.title = "Failed :("
-                alert.message = msg
+                println("Failed push!")
+                //alert.title = "Failed :("
+                //alert.message = msg
             }
             
-            // Move to the UI thread
+            /*// Move to the UI thread
             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                 // Show the alert
                 alert.show()
-            })
+            })*/
         }
     
     }
