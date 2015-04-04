1861843a115cec1261d5dfaf22589a7a49028a34
Jun Huang <mightybeesun@gmail.com>
2015-04-04 02:28:36 -0400

    sleep mode turn off and enhance battery life efficiency on thread

--- locomania/ViewController.swift
+++ locomania/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
 
     @IBOutlet weak var LatValue: UILabel!
     @IBOutlet weak var LongValue: UILabel!
-    @IBOutlet weak var Timer: UILabel!
+    @IBOutlet weak var TimestampOfLastPush: UILabel!
+    @IBOutlet weak var PushCountLabel: UILabel!
+    
     
     var locationManager: CLLocationManager!
     
     var currUserLocation: CLLocation!
-    
-    var currTimeSinceLastPush: Int!
     
     let PUSH_INTERVAL = 60*5;  //Time in seconds for how long between push
     
@@ -31,17 +31,14 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
                 // Do any additional setup after loading the view, typically from a nib.
         self.setUpLocationManager();
         
-        currTimeSinceLastPush = 0;
         pushCount = 0;
         
-        self.timerUpdate();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     //sets up location manager, and ask user for permission to use app location.
     func setUpLocationManager(){
@@ -59,30 +56,45 @@ class ViewController: UIViewController , CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!){
         var location:CLLocation = locations[locations.count-1] as CLLocation;
         println("locations = \(locations)");
-        LatValue.text = "\(location.coordinate.latitude)";
-        LongValue.text = "\(location.coordinate.longitude)";
-        currUserLocation = location;
+        self.LatValue.text = "\(location.coordinate.latitude)";
+        self.LongValue.text = "\(location.coordinate.longitude)";
+        let bInitPush = self.currUserLocation == nil
+        self.currUserLocation = location;
+        
+        if(bInitPush){
+            self.timerUpdate()
+        }
     }
     
     
     //recursive function that starts background thread every other 1 seconds to serve the time.
     func timerUpdate(){
-        let delay = 1 * Double(NSEC_PER_SEC);
+        self.pushLocationToServer(self.currUserLocation);
+        self.TimestampOfLastPush.text = self.getCurrentTimestamp();
+        self.PushCountLabel.text = "Total PushCount: \(self.pushCount)"
+        let delay = Double(self.PUSH_INTERVAL) * Double(NSEC_PER_SEC);
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay));
         dispatch_after(time, dispatch_get_main_queue()){
-            self.currTimeSinceLastPush = self.currTimeSinceLastPush + 1;
-            if (self.currTimeSinceLastPush > self.PUSH_INTERVAL ){
-                self.currTimeSinceLastPush = 0;
-                self.pushLocationToServer(self.currUserLocation);
-            }
-            self.Timer.text = String(self.currTimeSinceLastPush);
             self.timerUpdate();
         }
     }
     
+    func getCurrentTimestamp() -> String {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components( .CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond , fromDate: date)
+        let currTime = NSCalendar.currentCalendar().dateFromComponents(components)
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.LongStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
+        
+        return dateFormatter.stringFromDate(currTime!)
+    }
+
+    
     func pushLocationToServer(location: CLLocation){
         self.pushCount = self.pushCount + 1;
-        let url = NSURL(string: "http://myurl:8888/pushLatLong/\(self.pushCount)/\(location.coordinate.latitude)/\(location.coordinate.longitude)")
+        let url = NSURL(string: "http://192.168.1.110:8888/pushLatLong/\(self.pushCount)/\(location.coordinate.latitude)/\(location.coordinate.longitude)")
         let request = NSURLRequest(URL: url!)
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
             println(NSString(data: data, encoding: NSUTF8StringEncoding))
